8dfcbfe286549b3e881a1cfaf482dac87cc3509f
AnuragRai <S10C-4484@vidyavihar.org>
2021-05-25 13:40:41 +0530

    Create update-tl.sh

--- templates/update-tl.sh
+++ templates/update-tl.sh
@@ -8,4 +8,7 @@ get_latest_version() {
     rev
 }
 get_latest_version
+mv tl-template old-tl-template
 sed -r "s/(version\s*=\s*).*/\1$(get_latest_version)/" old-tl-template >> tl-template
+rm -rf old-tl-template
+rm -rf TL*
